3dc96f593c7b3ef674b3bcaba54c5161de74f588
Wikid82 <jhatfield82@gmail.com>
2026-09-21 09:33:22 -0400

    feat(scripts): add Kometa Docker run scripts for all libraries

---
+++ scripts/tvshows_only/delete_collections.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -a
+source "$(dirname "$0")/../.env"
+set +a
+
+docker run --name kometa_run  --rm -it -v "${KOMETA_CONFIG_DIR}:/config:rw" -e ${TZ} kometateam/kometa:nightly --run-libraries "TV Shows" --delete-collections --collections-only  --run
